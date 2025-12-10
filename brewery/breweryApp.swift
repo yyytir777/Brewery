3968006870e9f287d8cab7181cd853e4a2a064a0
Wonjae Lim <yyytir777@gmail.com>
2025-12-11 00:52:10 +0900

    Initial Commit

---
+++ brewery/breweryApp.swift
@@ -0,0 +1,17 @@
+//
+//  breweryApp.swift
+//  brewery
+//
+//  Created by Wonjae Lim on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct breweryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
